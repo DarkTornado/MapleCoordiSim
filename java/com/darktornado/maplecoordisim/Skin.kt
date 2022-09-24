01eade204214d6ef25b8e4f4f4d9494ef1d25dbd
Dark Tornado <dt3141592@naver.com>
2022-09-24 21:36:24 +0900

    피부 목록

---
+++ java/com/darktornado/maplecoordisim/Skin.kt
@@ -0,0 +1,27 @@
+package com.darktornado.maplecoordisim
+
+object Skin {
+    const val DEFAULT_ID = 8
+    val list = arrayOf(
+        Item("크림 피부", 2000, "skin"),
+        Item("태닝 피부", 2001, "skin"),
+        Item("헬시 피부", 2002, "skin"),
+        Item("밀키 피부", 2003, "skin"),
+        Item("스산 피부", 2004, "skin"),
+        Item("창백 피부", 2009, "skin"),
+        Item("노블레스 피부", 2010, "skin"),
+        Item("고져스 피부", 2011, "skin"),
+        Item("엘프 피부", 2012, "skin"),
+        Item("데모닉 피부", 2013, "skin"),
+        Item("뽀송 꽃잎 피부", 2015, "skin"),
+        Item("홍조 꽃잎 피부", 2016, "skin"),
+        Item("뽀송 라벤더 피부", 2018, "skin"),
+        Item("홍조 라벤더 피부", 2010, "skin"),
+        Item("옥구슬 피부", 2024, "skin"),
+        Item("금구슬 피부", 2025, "skin"),
+        Item("은구슬 피부", 2026, "skin"),
+        Item("동구슬 피부", 2027, "skin"),
+        Item("스피넬 피부", 2028, "skin"),
+        Item("아메지스트 피부", 2029, "skin")
+    )
+}
