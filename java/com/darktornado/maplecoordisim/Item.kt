d44e4ebe97050569fed0473e0f5b1c414da77203
Dark Tornado <dt3141592@naver.com>
2022-09-24 21:36:29 +0900

    Create Item.kt

---
+++ java/com/darktornado/maplecoordisim/Item.kt
@@ -0,0 +1,39 @@
+package com.darktornado.maplecoordisim
+
+import org.json.JSONObject
+import java.util.*
+
+class Item {
+    var name: String
+    var id: String
+    var type: String
+
+    constructor(name: String, id: String, type: String) {
+        this.name = name
+        this.id = id
+        this.type = type
+    }
+
+    constructor(name: String, id: Int, type: String) {
+        this.name = name
+        this.id = id.toString()
+        this.type = type
+    }
+
+    companion object {
+        const val VERSION = 366
+
+        @Throws(Exception::class)
+        fun createList(obj: JSONObject): HashMap<String, Item> {
+            val map =
+                HashMap<String, Item>()
+            val keys = obj.keys()
+            while (keys.hasNext()) {
+                val key = keys.next()
+                val item = obj.getJSONObject(key)
+                map[key] = Item(key, item.getString("id"), item.getString("type"))
+            }
+            return map
+        }
+    }
+}
