1f80b48f173b73313dab770cc8599e430eb81c31
Dark Tornado <dt3141592@naver.com>
2022-09-24 21:34:06 +0900

    Update MainActivity.kt

--- java/com/darktornado/maplecoordisim/MainActivity.kt
+++ java/com/darktornado/maplecoordisim/MainActivity.kt
@@ -2,10 +2,14 @@ package com.darktornado.maplecoordisim
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val layout = LinearLayout(this)
+        layout.orientation = 1
 
+        setContentView(layout)
     }
 }
