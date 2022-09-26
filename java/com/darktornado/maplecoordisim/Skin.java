7e7d58696f4c1a37670e8d26ea35796d8fcb7f02
Dark Tornado <dt3141592@naver.com>
2022-09-26 20:18:05 +0900

    자바로 변경

---
+++ java/com/darktornado/maplecoordisim/Skin.java
@@ -0,0 +1,29 @@
+package com.darktornado.maplecoordisim;
+
+class Skin {
+
+    public static final int DEFAULT_ID = 8;
+
+    public static Item[] list = {
+            new Item("크림 피부", 2000, "skin"),
+            new Item("태닝 피부", 2001, "skin"),
+            new Item("헬시 피부", 2002, "skin"),
+            new Item("밀키 피부", 2003, "skin"),
+            new Item("스산 피부", 2004, "skin"),
+            new Item("창백 피부", 2009, "skin"),
+            new Item("노블레스 피부", 2010, "skin"),
+            new Item("고져스 피부", 2011, "skin"),
+            new Item("엘프 피부", 2012, "skin"),
+            new Item("데모닉 피부", 2013, "skin"),
+            new Item("뽀송 꽃잎 피부", 2015, "skin"),
+            new Item("홍조 꽃잎 피부", 2016, "skin"),
+            new Item("뽀송 라벤더 피부", 2018, "skin"),
+            new Item("홍조 라벤더 피부", 2010, "skin"),
+            new Item("옥구슬 피부", 2024, "skin"),
+            new Item("금구슬 피부", 2025, "skin"),
+            new Item("은구슬 피부", 2026, "skin"),
+            new Item("동구슬 피부", 2027, "skin"),
+            new Item("스피넬 피부", 2028, "skin"),
+            new Item("아메지스트 피부", 2029, "skin")
+    };
+}
