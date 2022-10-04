ae86a551f33ebf34dc3791b3d9cf108e4af094dd
Dark Tornado <dt3141592@naver.com>
2022-10-04 09:32:28 +0900

    Update MainActivity.kt

--- java/com/darktornado/maplecoordisim/MainActivity.kt
+++ java/com/darktornado/maplecoordisim/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.darktornado.maplecoordisim
 
-import android.R
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.AdapterView
 import android.widget.AdapterView.OnItemClickListener
@@ -20,6 +21,22 @@ class MainActivity : AppCompatActivity() {
     private val names = ArrayList<String>()
     private var items: HashMap<String, Item>? = null
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menu.add(0, 0, 0, "아이템 추가")
+        menu.add(0, 1, 0, "피부색 변경")
+        menu.add(0, 2, 0, "새로 고침")
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+//            0 -> inputName()
+//            1 -> selectSkin()
+            2 -> mc!!.update()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val layout = LinearLayout(this)
@@ -29,7 +46,7 @@ class MainActivity : AppCompatActivity() {
         layout.addView(mc!!.image)
 
         val list = ListView(this)
-        adapter = ArrayAdapter<Any?>(this, R.layout.simple_list_item_1, names as List<Any?>)
+        adapter = ArrayAdapter<Any?>(this, android.R.layout.simple_list_item_1, names as List<Any?>)
         list.adapter = adapter
         list.onItemClickListener = OnItemClickListener { parent: AdapterView<*>?, view: View?, pos: Int, id: Long ->
         }
@@ -41,4 +58,5 @@ class MainActivity : AppCompatActivity() {
             items = Item.createList(JSONObject(_list))
         }).start()
     }
+
 }
