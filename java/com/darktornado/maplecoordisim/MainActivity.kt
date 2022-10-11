0cc48d03ce0e7ad2b8d27c3be99467a973b399fe
Dark Tornado <dt3141592@naver.com>
2022-10-11 23:11:59 +0900

    아이템 아이콘도 표시

--- java/com/darktornado/maplecoordisim/MainActivity.kt
+++ java/com/darktornado/maplecoordisim/MainActivity.kt
@@ -3,12 +3,17 @@ package com.darktornado.maplecoordisim
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.os.StrictMode
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemClickListener
+import com.darktornado.library.ListAdapter
+import com.darktornado.library.ListItem
 import com.darktornado.library.LoadingDialog
 import org.json.JSONObject
 import java.util.*
@@ -16,11 +21,12 @@ import java.util.*
 class MainActivity : Activity() {
 
     private val ITEM_LIST_URL = "https://raw.githubusercontent.com/DarkTornado/MapleCoordiSim/main/maple.json"
-    private var adapter: ArrayAdapter<*>? = null
+    private var adapter: ListAdapter? = null
     private var mc: MapleChar? = null
     private val names = ArrayList<String>()
     private val parts = ArrayList<String>()
-    private var items: HashMap<String, Item>? = null
+    private var itemList: HashMap<String, Item>? = null
+    private val items: ArrayList<ListItem> = ArrayList<ListItem>()
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menu.add(0, 0, 0, "아이템 추가")
@@ -47,15 +53,10 @@ class MainActivity : Activity() {
         layout.addView(mc!!.image)
 
         val list = ListView(this)
-        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, names as List<Any?>)
-        list.adapter = adapter
+        adapter = ListAdapter()
+        adapter!!.setItems(items)
+        list.adapter = adapter!!
         list.onItemClickListener = OnItemClickListener { parent: AdapterView<*>?, view: View?, pos: Int, id: Long ->
-            val type = parts[id.toInt()]
-            val name = mc!!.items.get(type)!!.name
-            mc!!.items.remove(type)
-            mc!!.update()
-            toast("$name(이)가 삭제되었어요.")
-            update()
         }
         layout.addView(list)
 
@@ -63,9 +64,10 @@ class MainActivity : Activity() {
         val dialog = LoadingDialog.create(this, "아이템 목록 불러오는 중...")
         Thread(Runnable {
             val _list: String = Utils.getWebText(ITEM_LIST_URL)
-            items = Item.createList(JSONObject(_list))
+            itemList = Item.createList(JSONObject(_list))
             runOnUiThread { dialog.dismiss() }
         }).start()
+        StrictMode.enableDefaults()
     }
     
     fun inputName() {
@@ -83,7 +85,7 @@ class MainActivity : Activity() {
         dialog.setNegativeButton("취소", null)
         dialog.setPositiveButton("검색") { _dialog: DialogInterface?, which: Int ->
             val input = txt.text.toString()
-            val item = items!![input]
+            val item = itemList!![input]
             if (item == null) {
                 searchItem(input)
             } else {
@@ -97,9 +99,9 @@ class MainActivity : Activity() {
 
     private fun searchItem(input: String) {
         val result = ArrayList<Item>()
-        val names: Set<String> = items!!.keys
+        val names: Set<String> = itemList!!.keys
         for (name in names) {
-            if (name.contains(input)) result.add(items!![name]!!)
+            if (name.contains(input)) result.add(itemList!![name]!!)
         }
         showSearhResult(result.toTypedArray())
     }
@@ -121,13 +123,28 @@ class MainActivity : Activity() {
         dialog.show()
     }
 
-    private fun update() {
-        names.clear()
-        parts.clear()
-        for (type in mc!!.items.keys) {
-            names.add(mc!!.items[type]!!.name)
-            parts.add(mc!!.items[type]!!.type)
+    fun showItemMenu(item: Item, icon: Drawable) {
+        val dialog = AlertDialog.Builder(this)
+        dialog.setTitle(item.name)
+        dialog.setIcon(icon)
+        dialog.setNegativeButton("닫기", null)
+        dialog.setPositiveButton("삭제") { _dialog: DialogInterface?, w: Int ->
+            mc!!.items.remove(item.type)
+            mc!!.update()
+            toast(item.name + "(이)가 삭제되었어요.")
+            update()
         }
+        dialog.show()
+    }
+
+    private fun update() {
+        items.clear()
+        for (type in mc!!.items.keys) {
+            val item = mc!!.items[type]
+            val icon: Drawable = BitmapDrawable(Utils.getWebImage("https://maplestory.io/api/KMS/" + Item.VERSION.toString() + "/item/" + item!!.id + "/icon"))
+            items.add(ListItem(item.name, item.type, icon))
+        }
+        adapter!!.setItems(items)
         adapter!!.notifyDataSetChanged()
     }
 
